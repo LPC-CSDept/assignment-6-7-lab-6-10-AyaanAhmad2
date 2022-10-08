7311d2a7fea0dc662f9b5651ff8b077f4108e318
Ayaan Ahmad <ayanahmad175@gmail.com>
2022-10-07 19:48:03 -0700

    this is so bruh

--- A67.hpp
+++ A67.hpp
@@ -3,7 +3,31 @@
 #include <iomanip>
 #include <iostream>
 using namespace std;
+void swap (int& n1, int& n2){
+    int temp = 0;
+  temp = n1;
+  //cout << temp << endl;
+  n1 = n2;
+  //cout << n1 << endl;
+  n2 = temp;
+ // cout << n2;
 
-// complete the Overloading function swap( )
-// swap ( ) function has the difference number of parameters from 2 to 4
-// For example, swap(10, 20); swap(10, 20, 30); swap(10, 20, 30, 40)
+}
+
+void swap (int& n1, int& n2, int& n3) {
+     int temp1 = 0;
+  temp1 = n3;
+     n3 = n2;
+     n2 = n1;
+     n1 = temp1;
+    
+}
+void swap (int& n1, int& n2, int& n3, int& n4) {
+   int temp2 = 0;
+   temp2 = n1;
+   n1 = n2;
+   n2 = n3;
+   n3 = n4;
+   n4 = temp2;
+}
+
